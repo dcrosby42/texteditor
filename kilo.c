3fe6acac7d1f1eae6223514d41c2c784d0820db7
David Crosby <dcrosby@llnw.com>
2017-04-14 00:50:56 -0400

    read timeout

--- kilo.c
+++ kilo.c
@@ -21,6 +21,9 @@ void enableRawMode() {
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // turn of echo, lineread, disable C-v and C-o, and C-c and C-z
   raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP);  // more misc input flags, obsolete
   raw.c_cflag |= (CS8);  // more misc input flags, obsolete
+  // "cc" means "control characters":
+  raw.c_cc[VMIN] = 0;  // min num bytes to read before returning
+  raw.c_cc[VTIME] = 1; // max block time for read(), in 0.1 second intervals (100ms)
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,13 +31,15 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
