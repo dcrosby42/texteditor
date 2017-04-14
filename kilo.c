eb147adf2327666fd7f02fc6201a4f4c48fd5b6a
David Crosby <dcrosby@llnw.com>
2017-04-14 00:38:25 -0400

    cleanup the rest of the Ctrl- keys

--- kilo.c
+++ kilo.c
@@ -15,8 +15,9 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  struct termios raw = orig_termios; // copy
+  raw.c_iflag &= ~(ICRNL | IXON);  // ICRNL makes C-m and Enter 13, and IXON disables C-q and C-s behavior
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // turn of echo, lineread, disable C-v and C-o, and C-c and C-z
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
