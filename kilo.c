1111a40d38ce5b81ef765942f994fc4940f10db8
David Crosby <dcrosby@llnw.com>
2017-04-14 00:46:12 -0400

    more io control flags to get us all the way into raw mode

--- kilo.c
+++ kilo.c
@@ -17,7 +17,10 @@ void enableRawMode() {
 
   struct termios raw = orig_termios; // copy
   raw.c_iflag &= ~(ICRNL | IXON);  // ICRNL makes C-m and Enter 13, and IXON disables C-q and C-s behavior
+  raw.c_oflag &= ~(OPOST);  // turn off "\n"-to-"\r\n" translation
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // turn of echo, lineread, disable C-v and C-o, and C-c and C-z
+  raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP);  // more misc input flags, obsolete
+  raw.c_cflag |= (CS8);  // more misc input flags, obsolete
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,9 +31,9 @@ int main() {
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
   return 0;
