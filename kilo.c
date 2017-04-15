29597f9d0be7d8c06a1d2b0433a92cf3497729de
David Crosby <dcrosby@llnw.com>
2017-04-15 13:48:45 -0400

    Windowsize fallback

--- kilo.c
+++ kilo.c
@@ -66,11 +66,32 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO,"\x1b[6n",4) != 4) return -1;
+
+  while (i < sizeof(buf) -1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws; // ioctl.h
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    // TIOCGWINSZ didn't work, let's move the cursor to see where it goes:
+    if (write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12) != 12) return -1; // C=Cursor Forward, B=Cursor Down
+    return getCursorPosition(rows,cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -83,7 +104,10 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n",3);
+    write(STDOUT_FILENO, "~",1);
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n",2);
+    }
   }
 }
 
