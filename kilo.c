0ec7c626a30635adb17fbde3cdf7eb9681e31669
David Crosby <dcrosby@llnw.com>
2017-04-15 20:17:56 -0400

    vert scroll

--- kilo.c
+++ kilo.c
@@ -43,6 +43,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx,cy;
+  int rowoff;
   int screenrows;
   int screencols;
   int numrows;
@@ -219,11 +220,20 @@ void abFree(struct abuf *ab) {
 }
 
 /*** output ***/
+void editorScroll() {
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
 
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y >= E.numrows) {
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
       // off the end of the text rows
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
@@ -241,9 +251,9 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~",1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K",3);
@@ -259,6 +269,8 @@ void editorClearScreen() {
 }
 
 void editorRefreshScreen() {
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   // Hide cursor and move to topleft
@@ -269,7 +281,7 @@ void editorRefreshScreen() {
 
   // Move cursor to stored pos
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx+1);
   abAppend(&ab, buf, strlen(buf));
 
   // show cursor
@@ -299,7 +311,7 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_DOWN:
-      if (E.cy != E.screenrows - 1) {
+      if (E.cy < E.numrows) {
         E.cy++;
       }
       break;
@@ -345,6 +357,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
   E.numrows = 0;
   E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
