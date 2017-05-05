bbe6d183f14701c6e107bc1584a4da25e2864011
David Crosby <dcrosby@llnw.com>
2017-05-04 23:52:39 -0400

    newline

--- kilo.c
+++ kilo.c
@@ -214,10 +214,12 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1); 
   memcpy(E.row[at].chars, s, len);
@@ -228,6 +230,22 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
   E.dirty++;
 }
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
 
 void editorFreeRow(erow *row) {
   free(row->render);
@@ -274,7 +292,7 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) { // cursor is on the ~ line after the end of the file
-    editorAppendRow("", 0); // start a new row
+    editorInsertRow(E.numrows, "", 0); // start a new row
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
@@ -334,7 +352,7 @@ void editorOpen(char *filename) {
     while (linelen > 0 && (line[linelen-1] == '\n' ||
                            line[linelen-1] == '\r'))
       linelen--;
-    editorAppendRow(line,linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -558,7 +576,7 @@ void editorProcessKeypress() {
   int c = editorReadKey();
   switch (c) {
     case '\r':
-      /* TODO */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
